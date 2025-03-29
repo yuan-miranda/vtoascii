13e39cc5b901fead271fd831886fd1a2439a1dea
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-03-29 22:32:38 +0800

    synced with the nascii repo

--- player.py
+++ player.py
@@ -2,6 +2,8 @@ import sys
 import os
 import time
 
+FILE = None
+
 def clear_console():
     if sys.platform.startswith('win'):
         _ = os.system('cls')
@@ -9,16 +11,34 @@ def clear_console():
         _ = os.system('clear')
 
 def main():
-    filenames = os.listdir("output")
-    for i, filename in enumerate(filenames):
-        print(f"{i}: {filename}")
+    global FILE
+    if len(sys.argv) > 1:
+        FILE = sys.argv[1]
+    elif FILE is None:
+        files = os.listdir("output")
+        if not files:
+            return print("No files found in /output directory.")
+        
+        for i, file in enumerate(files):
+            print(f"{i}: {file}")
+        
+        try:
+            choice = int(input("> "))
+            if choice < 0 or choice >= len(files):
+                raise ValueError()
+            time.sleep(0.5)
+            FILE = files[choice]
+        except ValueError:
+            return print(f"Invalid or out of range. Selection must be between 0 and {len(files) - 1}.")
 
-    choice = int(input("> "))
-    time.sleep(0.5)
-    filename = filenames[choice]
-    filename = os.path.join("output", filename, f"{os.path.splitext(filename)[0]}.txt")
+    
+    if not os.path.exists(f"media/{FILE}"):
+        return print(f"{FILE} does not exist.")
 
-    with open(filename, "r") as f:
+    file_name = os.path.splitext(FILE)[0]
+    FILE = os.path.join("output", file_name, f"{file_name}.txt")
+
+    with open(FILE, "r") as f:
         lines = f.readlines()
         for line in lines:
             if line == "END\n":
--- vtoascii.py
+++ vtoascii.py
@@ -4,25 +4,42 @@ from PIL import Image
 import time
 from main import to_frames, resize_image, quantize_image, to_ascii
 
-WIDTH = 32*2
+WIDTH = 32
 HEIGHT_PERCENTAGE = 0.38
 BIT_DEPTH = 8
 FILE = None
 
 def main():
     global FILE
-    if FILE is None:
-        filenames = os.listdir("media")
-        for i, filename in enumerate(filenames):
-            print(f"{i}: {filename}")
+    if len(sys.argv) > 1:
+        FILE = sys.argv[1]
+    elif FILE is None:
+        files = os.listdir("media")
+        if not files:
+            return print("No files found in /media directory.")
         
-        choice = int(input("> "))
-        time.sleep(0.5)
-        FILE = filenames[choice]
+        for i, file in enumerate(files):
+            print(f"{i}: {file}")
+        
+        try:
+            choice = int(input("> "))
+            if choice < 0 or choice >= len(files):
+                raise ValueError()
+            time.sleep(0.5)
+            FILE = files[choice]
+        except ValueError:
+            return print(f"Invalid or out of range. Selection must be between 0 and {len(files) - 1}.")
 
-    output_path = f"output/{os.path.splitext(FILE)[0]}"
-    output_path_frames = f"output/{os.path.splitext(FILE)[0]}/frames"
+    if not os.path.exists(f"media/{FILE}"):
+        return print(f"{FILE} does not exist.")
 
+    file_name = os.path.splitext(FILE)[0]
+
+    if os.path.exists(f"output/{file_name}"):
+        return print(f"{file_name} already converted.")
+
+    output_path = f"output/{file_name}"
+    output_path_frames = f"{output_path}/frames"
     os.makedirs(output_path_frames, exist_ok=True)
 
     time_start = time.time()
@@ -44,11 +61,11 @@ def main():
         quantize_image(pixels, width, height, grayscale=True)
         img.save(f"{output_path_frames}/{i}")
     
-    for i in frames:
-        img = Image.open(f"{output_path_frames}/{i}")
-        width, height = img.size
-        ascii_img = to_ascii(img.load(), width, height)
-        with open(f"{output_path}/{os.path.splitext(FILE)[0]}.txt", "a") as f:
+    with open(f"{output_path}/{file_name}.txt", "w") as f:
+        for i in frames:
+            img = Image.open(f"{output_path_frames}/{i}")
+            width, height = img.size
+            ascii_img = to_ascii(img.load(), width, height)
             f.write(ascii_img)
             f.write("END\n")
 
